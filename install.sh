ce0e9f0ab8bc7a0832beb4cb106d1dd84e4ceb94
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2023-03-15 03:02:31 +0100

    Fix root password, network, and boot subvolume

--- install.sh
+++ install.sh
@@ -16,13 +16,15 @@ btrfs filesystem label $LINUX_PART arch
 mount -o subvol=@ $LINUX_PART /mnt
 mount --mkdir $BOOT_PART /mnt/boot
 
-pacstrap -K /mnt base linux linux-firmware
+pacstrap -K /mnt base base-devel linux linux-firmware networkmanager
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "root:root" | chpasswd
+echo "root:root" | arch-chroot /mnt chpasswd
+
+arch-chroot /mnt systemctl enable NetworkManager
+arch-chroot /mnt systemctl enable fstrim.timer
 
 bootctl --esp-path=/mnt/boot install
-
 cp ./loader.conf /mnt/boot/loader/loader.conf
 cp ./arch.conf /mnt/boot/loader/entries/arch.conf
