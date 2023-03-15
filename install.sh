c9b3905ccd5306e1f28c7e89a09a52157af7d2bb
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2023-03-15 13:56:34 +0100

    add setup script

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 
 BOOT_PART=
 LINUX_PART=
+ROOT_PASS=
 
 mkfs.fat -F 32 $BOOT_PART
 
@@ -11,18 +12,20 @@ mkfs.btrfs $LINUX_PART
 
 mount $LINUX_PART /mnt
 btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
 umount /mnt
 
 btrfs filesystem label $LINUX_PART arch
 
 mount -o subvol=@ $LINUX_PART /mnt
+mount -o subvol=@home $LINUX_PART /mnt/home
 mount --mkdir $BOOT_PART /mnt/boot
 
 pacstrap -K /mnt base base-devel linux linux-firmware networkmanager
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "root:root" | arch-chroot /mnt chpasswd
+echo "root:$ROOT_PASS" | arch-chroot /mnt chpasswd
 
 arch-chroot /mnt systemctl enable NetworkManager
 arch-chroot /mnt systemctl enable fstrim.timer
--- setup.sh
+++ setup.sh
@@ -1,1 +1,93 @@
 #/bin/sh
+
+set -e
+
+USER_NAME=
+USER_PASS=
+DOTFILES_REPO=
+
+function run_as_user() {
+  sudo --user $USER_NAME $@
+}
+
+function run_as_user_chdir() {
+  cd $1
+  sudo --user $USER_NAME ${$@:2}
+  cd -
+}
+
+pacman -Syu --noconfirm
+
+pacman -S --noconfirm git fish sudo btrfs-progs
+
+chmod 640 /etc/sudoers
+echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+useradd --shell /bin/fish --create-home --btrfs-subvolume-home $USER_NAME
+usermod --append --groups wheel $USER_NAME
+echo "$USER_NAME:$USER_PASS" | chpasswd
+
+run_as_user mkdir /home/$USER_NAME/Downloads
+
+run_as_user_chdir /home/$USER_NAME/Downloads git clone https://aur.archlinux.org/paru-bin
+run_as_user_chdir /home/$USER_NAME/Downloads/paru-bin makepkg --syncdeps --install --noconfirm
+
+pacman -S --noconfirm pipewire pipewire-audio wireplumber pipewire-alsa pipewire-pulse pipewire-jack pavucontrol
+
+pacman -S --noconfirm bluez bluez-utils
+systemctl enable bluetooth
+
+pacman -S --noconfirm xorg-server xorg-xinit
+
+pacman -S --noconfirm lightdm lightdm-gtk-greeter
+systemctl enable lightdm
+
+pacman -S --noconfirm i3-wm
+
+run_as_user paru -S --noconfirm dfm
+run_as_user git clone $DOTFILES_REPO /home/$USER_NAME/.df
+run_as_user dfm apply --force
+
+sudo --user $USER_NAME paru -S --noconfirm \
+  pacman-contrib \
+  man-db \
+  firefox \
+  neovim \
+  visual-studio-code-bin \
+  libreoffice \
+  evince \
+  spotify \
+  slack-desktop \
+  signal-desktop \
+  discord \
+  jdk8-openjdk \
+  jdk-openjdk \
+  rustup \
+  alacritty \
+  bat \
+  bottom \
+  git-delta \
+  exa \
+  fd \
+  ripgrep \
+  starship \
+  tealdeer \
+  tokei \
+  zoxide \
+  zellij \
+  difftastic \
+  ncdu \
+  vlc \
+  thunar \
+  flameshot \
+  sl \
+  openssh \
+  cowfortune \
+  cowsay \
+  lolcat \
+  polybar \
+  lsp-plugins \
+  picom \
+  rofi \
+  zip \
+  unzip
